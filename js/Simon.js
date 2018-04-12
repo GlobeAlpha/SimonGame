82544f61733489ff8a96be344ca157c68ee2e1d6
GlobeAlpha <maciekbajda92@hotmail.com>
2018-04-12 12:46:38 -0400

    First Commit

---
+++ js/Simon.js
@@ -0,0 +1,57 @@
+power = false;
+//TODO Make Random  sounds play after user starts the game.
+//$("#powerSwitch").click(function()
+//{ 
+
+//if (power == false) {// makes Off switch dissapear and leaves the  On switch
+    
+    
+//    console.log(power);
+//  $("button").removeClass("Player");
+//  $("button").addClass("Active")
+//  $("#buttonOn").css("visibility", "visible");
+//  $("#buttonOff").css("visibility", "hidden");
+  
+//  //$("#displayText").css("opacity","1");
+//  $("#displayText").html("--");
+ 
+  
+//  $("#startBT").css("background-color", "#39FF14");
+
+
+
+//  power = true;
+ 
+//}
+ 
+//else if (power == true) {// hides ON switch and displays OFF switch 
+    
+//    console.log(power);
+//    $("button").removeClass("Active");
+//    $("button").addClass("Player");
+//    $("#buttonOn").css("visibility", "hidden");
+//    $("#buttonOff").css("visibility", "visible");
+    
+   
+  
+//  $("#startBT").css("background-color", "red");
+  
+//    //power = true;
+//  //running = false;
+ 
+//strict="off";   
+//power = false;
+//  $("#strictBT").css("background", "yellow");
+
+//    }
+
+////$('#buttonOn').click(function ()
+////{
+////        $(this).toggleClass("down");
+////    });
+
+//});
+
+
+//});
+
